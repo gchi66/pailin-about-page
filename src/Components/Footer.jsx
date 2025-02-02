81c18b97571141c2d824bdc6d863c10ff8c37d89
grant chichester <gchichester@gmail.com>
2025-02-02 12:26:25 -0600

    changed copyright year

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,9 @@ import "../Styles/Footer.css";
 const Footer = ({ isAccordionOpen }) => {
   return (
     <div className={`footer-container ${!isAccordionOpen ? "fixed-bottom" : ""}`}>
-      <p className="footer-title">Copyright © 2024, Pailin Abroad</p>
+      <p className="footer-title">
+        Copyright © {new Date().getFullYear()}, Pailin Abroad
+      </p>
     </div>
   );
 };
